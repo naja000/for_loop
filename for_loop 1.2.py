ce2f3feba722309a96910e5b1e3b4dee64806efc
naja000 <110969361+naja000@users.noreply.github.com>
2022-08-25 14:47:16 +0530

    Add files via upload

---
+++ for_loop 1.2.py
@@ -0,0 +1,11 @@
+num=2
+for i in range(1,11):
+    print(i,"x",num,"=",i*num)
+
+print("_____________________")
+
+for num in range(-10,0,1):
+    print(num)
+print("_____________________")
+for num in range(-9, 0, 2):
+    print(num)
